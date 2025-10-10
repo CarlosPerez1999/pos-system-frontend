70ad31cfd93dfa68d6552c9cbf0c72e997e6959a
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-10 11:03:06 -0600

    feat(products): add product card component with image and price display

---
+++ src/app/features/pos/products/components/product-card/product-card.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { Product } from '../../../../../core/models/product.model';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'product-card',
+  imports: [CurrencyPipe],
+  templateUrl: './product-card.html',
+})
+export class ProductCard {
+  product = input.required<Product>()
+}

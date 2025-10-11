8482ebb407c1211ee1475defbb9b460c3742441c
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-11 02:42:15 -0600

    feat(products): implement add to cart functionality in product card component

--- src/app/features/products/components/product-card/product-card.ts
+++ src/app/features/products/components/product-card/product-card.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../../../../core/models/product.model';
 import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../../sales/services/cart-service';
 
 @Component({
   selector: 'product-card',
@@ -8,5 +9,11 @@ import { CurrencyPipe } from '@angular/common';
   templateUrl: './product-card.html',
 })
 export class ProductCard {
-  product = input.required<Product>()
+  product = input.required<Product>();
+  cartService = inject(CartService);
+
+  addProductToCart() {
+    this.cartService.addProduct(this.product());
+    console.log(this.cartService.getProducts());
+  }
 }

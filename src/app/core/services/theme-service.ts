96ce87884ac4f759d56b468cb0c92c236dcbd875
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-11 17:00:31 -0600

    feat(core): add theme service to manage light and dark mode themes

---
+++ src/app/core/services/theme-service.ts
@@ -0,0 +1,35 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private storageThemeKey = 'theme';
+  isDarkMode = signal<boolean>(this.getInitialTheme());
+
+  constructor() {
+    this.applyTheme(this.isDarkMode());
+  }
+
+  private applyTheme(isDark: boolean) {
+    const theme = isDark ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  private getInitialTheme(): boolean {
+    const storedTheme = localStorage.getItem(this.storageThemeKey);
+    if (storedTheme === 'dark') return true;
+    if (storedTheme === 'light') return false;
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  toggleTheme() {
+    const newThemeIsDark = !this.isDarkMode();
+    this.isDarkMode.set(newThemeIsDark);
+    this.applyTheme(newThemeIsDark);
+    localStorage.setItem(
+      this.storageThemeKey,
+      newThemeIsDark ? 'dark' : 'light'
+    );
+  }
+}

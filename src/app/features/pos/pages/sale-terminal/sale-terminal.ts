dde41b403349b9479829345c60a583e0227b7915
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-11 02:42:59 -0600

    feat(sales): enhance sale terminal layout with shopping cart and responsive design

--- src/app/features/pos/pages/sale-terminal/sale-terminal.ts
+++ src/app/features/pos/pages/sale-terminal/sale-terminal.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { ProductsGrid } from '../../../products/components/products-grid/products-grid';
+import { ShoppingCart } from '../../../sales/components/shopping-cart/shopping-cart';
 
 @Component({
   selector: 'app-sale-terminal',
-  imports: [ProductsGrid],
+  imports: [ProductsGrid, ShoppingCart],
   templateUrl: './sale-terminal.html',
 })
-export class SaleTerminal {
-
-}
+export class SaleTerminal {}

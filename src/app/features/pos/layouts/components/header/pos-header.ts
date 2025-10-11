680503964361091790e0a9eb515572b3fad22fd2
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-11 17:00:46 -0600

    feat(header): add theme toggle functionality to header component

--- src/app/features/pos/layouts/components/header/pos-header.ts
+++ src/app/features/pos/layouts/components/header/pos-header.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AppButton } from "../../../../../shared/components/app-button/app-button";
+import { ThemeService } from '../../../../../core/services/theme-service';
 
 @Component({
   selector: 'pos-header',
   imports: [AppButton],
   templateUrl: './pos-header.html',
 })
-export class Header {}
+export class Header {
+  themeService = inject(ThemeService)
+}

8c54211c0403c220eecc4d2fcf836b5fb0fa4ebf
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-10 20:18:44 -0600

    feat(pos): add POS header component with template and styles

---
+++ src/app/features/pos/layouts/components/header/pos-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { AppButton } from "../../../../../shared/components/app-button/app-button";
+
+@Component({
+  selector: 'pos-header',
+  imports: [AppButton],
+  templateUrl: './pos-header.html',
+})
+export class Header {}

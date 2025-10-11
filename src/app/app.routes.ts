945660d1a9de39b6276c011a00a16f2d60a13207
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-10 20:20:33 -0600

    feat(routes): add lazy loading for POS feature module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pos',
+    loadChildren:() => import('./features/pos/pos.routes').then(m => m.POS_ROUTES) 
+  }
+];

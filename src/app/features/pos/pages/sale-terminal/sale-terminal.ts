ca941c10ce9d0d958b83023f07e3a09551cdd4be
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-10 20:19:52 -0600

    feat(pos): add sale terminal component with products grid integration

---
+++ src/app/features/pos/pages/sale-terminal/sale-terminal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProductsGrid } from "../../products/components/products-grid/products-grid";
+
+@Component({
+  selector: 'app-sale-terminal',
+  imports: [ProductsGrid],
+  templateUrl: './sale-terminal.html',
+})
+export class SaleTerminal {
+
+}

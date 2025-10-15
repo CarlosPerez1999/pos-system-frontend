cf3afea1560e53c3c07786f1587db514e661fbd9
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-15 13:55:56 -0600

    feat(pos): implement sale confirmation modal in sale terminal page

--- src/app/features/pos/pages/sale-terminal/sale-terminal.ts
+++ src/app/features/pos/pages/sale-terminal/sale-terminal.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ProductsGrid } from '../../../products/components/products-grid/products-grid';
 import { ShoppingCart } from '../../../sales/components/shopping-cart/shopping-cart';
+import { AppModal } from '../../../../shared/components/app-modal/app-modal';
+import { CartService } from '../../../sales/services/cart-service';
+import { CurrencyPipe } from '@angular/common';
+import { AppButton } from '../../../../shared/components/app-button/app-button';
+import { ModalService } from '../../../../core/services/modal-service';
 
 @Component({
   selector: 'app-sale-terminal',
-  imports: [ProductsGrid, ShoppingCart],
+  imports: [
+    ProductsGrid,
+    ShoppingCart,
+    AppModal,
+    FormsModule,
+    CurrencyPipe,
+    AppButton,
+  ],
   templateUrl: './sale-terminal.html',
 })
-export class SaleTerminal {}
+export class SaleTerminal {
+  cartService = inject(CartService);
+  modalService = inject(ModalService);
+  customerPayment = model<number>(NaN);
+
+  calculateChange = computed(() => {
+    const payment = this.customerPayment();
+    const total = this.cartService.total();
+
+    if (isNaN(payment) || payment <= total) return 0;
+    return payment - total;
+  });
+
+  cancelSale() {
+    this.customerPayment.set(0);
+    this.modalService.closeModal('sale-confirmation');
+  }
+}

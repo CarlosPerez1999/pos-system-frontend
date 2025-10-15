f84dc606c55cb00fd961d0d5d31108ece8c1b505
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-15 15:45:02 -0600

    feat(app-modal): add onClose output property and implement close method for modal functionality

--- src/app/shared/components/app-modal/app-modal.ts
+++ src/app/shared/components/app-modal/app-modal.ts
@@ -1,4 +1,11 @@
-import { Component, computed, inject, input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
 import { ModalService } from '../../../core/services/modal-service';
 import { AppButton } from '../app-button/app-button';
 import { NgClass } from '@angular/common';
@@ -12,6 +19,7 @@ export class AppModal {
   modalId = input.required<string>();
   title = input<string>('');
   modalService = inject(ModalService);
+  onClose = output();
 
   type = input<'info' | 'error' | 'warning' | 'success'>('error');
 
@@ -25,4 +33,9 @@ export class AppModal {
   titleStyle = computed(() => {
     return `${this.types[this.type()]}`;
   });
+
+  close() {
+    this.modalService.closeModal(this.modalId());
+    this.onClose.emit();
+  }
 }

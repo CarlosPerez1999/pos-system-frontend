6c8d11d50732b8a5577af00519332148443fe04c
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-15 22:09:00 -0600

    feat: add AppToast component to the main app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,17 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './core/services/theme-service';
 import { ModalService } from './core/services/modal-service';
+import { AppToast } from './shared/components/app-toast/app-toast';
+import { ToastService } from './core/services/toast-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppToast],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   themeService = inject(ThemeService);
   modalService = inject(ModalService);
+  toastService = inject(ToastService);
 }

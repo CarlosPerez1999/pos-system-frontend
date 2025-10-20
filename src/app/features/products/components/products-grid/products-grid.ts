5fff8bab71360df9ec1c1614a685662fcfd915da
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-20 10:22:21 -0600

    feat(products): implement infinite scrolling for products grid using IntersectionObserver

--- src/app/features/products/components/products-grid/products-grid.ts
+++ src/app/features/products/components/products-grid/products-grid.ts
@@ -1,4 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  ElementRef,
+  inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { ProductsService } from '../../services/products-service';
 import { ProductCard } from '../product-card/product-card';
 
@@ -9,4 +17,19 @@ import { ProductCard } from '../product-card/product-card';
 })
 export class ProductsGrid {
   productsService = inject(ProductsService);
+
+  sentinelRef = viewChild<ElementRef<HTMLDivElement>>('sentinel');
+
+  observer = effect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && this.productsService.hasMore()) {
+          this.productsService.nextPage();
+        }
+      });
+    });
+    const sentinel = this.sentinelRef();
+    if (!sentinel?.nativeElement) return;
+    observer.observe(sentinel.nativeElement);
+  });
 }

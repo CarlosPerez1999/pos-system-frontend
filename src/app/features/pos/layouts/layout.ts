f26c11a98da97bfe1afba7a91437d552b0b2615c
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-20 13:14:18 -0600

    refactor: rename pos-layout.ts to layout.ts and update template references

--- src/app/features/pos/layouts/layout.ts
+++ src/app/features/pos/layouts/layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Header } from "../components/header/pos-header";
 import { RouterOutlet } from '@angular/router';
+import { AppHeader } from '../../../shared/components/app-header/app-header';
 
 @Component({
   selector: 'app-pos-layout',
-  imports: [Header, RouterOutlet ],
-  templateUrl: './pos-layout.html',
+  imports: [RouterOutlet, AppHeader ],
+  templateUrl: './layout.html',
 })
 export default class PosLayout {
 
--- src/app/features/pos/pos.routes.ts
+++ src/app/features/pos/pos.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import PosLayout from './layouts/pos-layout/pos-layout';
+import PosLayout from './layouts/layout';
 import { SaleTerminal } from './pages/sale-terminal/sale-terminal';
 
 export const POS_ROUTES: Routes = [

562376375598e86122bbf62b7c883fc745c38300
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-21 08:10:14 -0600

    feat(products): implement infinite scroll for products grid component

--- src/app/features/products/components/products-grid/products-grid.ts
+++ src/app/features/products/components/products-grid/products-grid.ts
@@ -9,27 +9,17 @@ import {
 } from '@angular/core';
 import { ProductsService } from '../../services/products-service';
 import { ProductCard } from '../product-card/product-card';
+import { AppInfiniteScroll } from '../../../../shared/directives/app-infinite-scroll';
 
 @Component({
   selector: 'products-grid',
-  imports: [ProductCard],
+  imports: [ProductCard, AppInfiniteScroll],
   templateUrl: './products-grid.html',
 })
 export class ProductsGrid {
   productsService = inject(ProductsService);
 
-  sentinelRef = viewChild<ElementRef<HTMLDivElement>>('sentinel');
-
-  observer = effect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && this.productsService.hasMore()) {
-          this.productsService.nextPage();
-        }
-      });
-    });
-    const sentinel = this.sentinelRef();
-    if (!sentinel?.nativeElement) return;
-    observer.observe(sentinel.nativeElement);
-  });
+  loadMore = () => {
+    this.productsService.nextPage();
+  };
 }

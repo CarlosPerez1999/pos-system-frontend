9c26676e59e8bec2a967779a6954929d4f26ddc2
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-10-22 18:27:39 -0600

    feat(admin-layout): create admin layout component with header and tab menu

---
+++ src/app/features/admin/layouts/layout.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppHeader} from '../../../shared/components/app-header/app-header';
+import { TabMenuItem, AppTabMenu } from '../../../shared/components/app-tab-menu/app-tab-menu';
+
+
+@Component({
+  selector: 'admin-layout',
+  imports: [RouterOutlet, AppHeader, AppTabMenu],
+  templateUrl: './layout.html',
+})
+export class AdminLayout {
+  tabMenuItems:TabMenuItem[] = [
+    {
+      label: 'Dashboard',
+      href: '/admin/dashboard',
+      iconName: 'ix:dashboard'
+    },
+    {
+      label: 'Products',
+      href: '/admin/products',
+      iconName: 'lucide:package'
+    },
+    {
+      label: 'Inventory',
+      href: '/admin/analytics',
+      iconName: 'material-symbols:inventory'
+    },
+    {
+      label: 'Users',
+      href: '/admin/users',
+      iconName: 'flowbite:users-solid'
+    },
+    {
+      label: 'Configuration',
+      href: '/admin/config',
+      iconName: 'mdi:gear'
+    },
+  ]
+
+}

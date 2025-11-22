e58212be7d484bb1e5cabd9941528628dab08643
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-11-22 12:48:38 -0600

    fix(toast): adjust icon mapping for toast types

--- src/app/shared/components/app-toast/app-toast.ts
+++ src/app/shared/components/app-toast/app-toast.ts
@@ -1,11 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
 import { ToastService } from '../../../core/services/toast-service';
 import { NgClass } from '@angular/common';
+import { AppIcon } from '../app-icon/app-icon';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, AppIcon],
   templateUrl: './app-toast.html',
 })
 export class AppToast {
@@ -21,6 +22,21 @@ export class AppToast {
     success: 'bg-success',
   };
 
+  toastIcon = computed(() => {
+    const type = this.toastData()?.type ?? 'info';
+    switch (type) {
+      case 'info':
+        return 'line-md:alert-circle';
+      case 'error':
+        return 'line-md:close-circle';
+      case 'warning':
+        return 'line-md:alert';
+      case 'success':
+        return 'line-md:circle-to-confirm-circle-transition';
+      default:
+        return 'line-md:alert-circle';
+    }
+  });
   toastStyle = computed(() => {
     const type = this.toastData()?.type ?? 'info';
     return this.typeClasses[type];
